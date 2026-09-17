78fa81111c0b3ad86f9742207423a0a0659819ee
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-09-17 15:01:41 -0400

    feat: add the ability to change schedules in-app feat: show the time left until school starts

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ import json
 import time
 import rumps
 import threading
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 
-VERSION = "0.1.0"
+VERSION = "0.2.0"
 
 schedules = {}
 json_path = Path(__file__).parent / "schedules"
 for schedule_json in json_path.glob("*.json"):
     schedules[schedule_json.name.split(".")[0]] = json.loads((schedule_json.read_text()))
+
+def get_title(timeframe, period, seconds):
+    title = f"{timeframe.upper()} ({period}): "
+    hours = seconds // 3600
+    minutes = (seconds // 60) % 60
+    if hours > 0:
+        title += f"{seconds // 3600}h "
+    if minutes > 0 or hours > 0:
+        title += f"{(seconds // 60) % 60}m "
+    title += f"{seconds % 60}s"
+    return title
 
 class ScheduleApp(rumps.App):
     def __init__(self):
@@ -21,8 +32,16 @@ class ScheduleApp(rumps.App):
             quit_button=None
         )
 
+        self.options = list(schedules.keys())
+        self.sub_items = {
+            opt: rumps.MenuItem(title=opt, callback=self.select_option)
+            for opt in self.options
+        }
+        self.selected_schedule = "default"
+        self.sub_items[self.selected_schedule].state = True
+
         self.menu = [
-            "Change Schedule",
+            ("Change Schedule", list(self.sub_items.values())),
             "About",
             rumps.separator,
             "Quit"
@@ -30,6 +49,11 @@ class ScheduleApp(rumps.App):
 
         self.us_thread = threading.Thread(target=self.update_schedule_loop, daemon=True)
         self.us_thread.start()
+
+    def select_option(self, sender):
+        self.sub_items[self.selected_schedule].state = False
+        self.selected_schedule = sender.title
+        sender.state = True
 
     def update_schedule_loop(self):
         while True:
@@ -42,20 +66,25 @@ class ScheduleApp(rumps.App):
                 start_delta = round((datetime.strptime(f"{month}/{day}/{year} {period["start"]}", "%m/%d/%Y %I:%M%p") - now).total_seconds())
                 end_delta = round((datetime.strptime(f"{month}/{day}/{year} {period["end"]}", "%m/%d/%Y %I:%M%p") - now).total_seconds())
                 if start_delta > 0:
-                    self.title = f"START ({period_id}): {start_delta // 60}m {start_delta % 60}s"
+                    self.title = get_title("start", period_id, start_delta)
                     found_period = True
                     break
                 elif end_delta > 0:
                     found_period = True
-                    self.title = f"END ({period_id}): {end_delta // 60}m {end_delta % 60}s"
+                    self.title = get_title("end", period_id, end_delta)
                     break
             if not found_period:
-                self.title = "School's out!"
+                tomorrow = datetime.today() + timedelta(days=1)
+                day = tomorrow.day
+                month = tomorrow.month
+                year = tomorrow.year
+                start_delta = round((datetime.strptime(f"{month}/{day}/{year} {schedules["3-assembly"][list(schedules["3-assembly"].keys())[0]]["start"]}", "%m/%d/%Y %I:%M%p") - now).total_seconds())
+                self.title = get_title("start", list(schedules[self.selected_schedule].keys())[0], start_delta)
             time.sleep(0.5)
 
     @rumps.clicked("About")
     def about(self, _):
-        rumps.alert(f"Schedule App v{VERSION}\n\nAn easy way to see how much time is left until your next class!\n\nCreated by Aaron (GH: aaronwijes)")
+        rumps.alert(f"Menu Bar Schedule v{VERSION}\n\nAn easy way to see how much time is left until your next class!\n\nCreated by Aaron (GH: aaronwijes)")
 
     @rumps.clicked("Quit")
     def quit_app(self, _):
