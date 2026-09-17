e6f6309e0245ee9897d1acb67a05ceb4382ebd98
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-09-17 12:36:25 -0400

    feat: store schedules to individual JSON files feat: automatically start updating the menu bar with the time remaining until the next period chore: update version to 0.1.0

--- main.py
+++ main.py
@@ -1,104 +1,35 @@
 import os
+import json
 import time
 import rumps
 import threading
 from datetime import datetime
+from pathlib import Path
 
-schedules = {
-    "regular": {
-        "1": {
-            "start": "8:00AM",
-            "end": "8:41AM"
-        },
-        "2": {
-            "start": "8:45AM",
-            "end": "9:26AM"
-        },
-        "3": {
-            "start": "9:30AM",
-            "end": "10:17AM" # 47 mins instead of 41 mins
-        },
-        "4": {
-            "start": "10:21AM",
-            "end": "11:02AM"
-        },
-        "5": {
-            "start": "11:06AM",
-            "end": "11:47AM"
-        },
-        "6": {
-            "start": "11:51AM",
-            "end": "12:32PM"
-        },
-        "7": {
-            "start": "12:36PM",
-            "end": "1:17PM"
-        },
-        "8": {
-            "start": "1:21PM",
-            "end": "2:02PM"
-        },
-        "9": {
-            "start": "2:06PM",
-            "end": "2:47PM"
-        }
-    },
-    "3-assembly": {
-        "1": {
-            "start": "8:00AM",
-            "end": "8:38AM"
-        },
-        "2": {
-            "start": "8:42AM",
-            "end": "9:20AM"
-        },
-        "3": {
-            "start": "9:24AM",
-            "end": "10:32AM" # 47 mins instead of 41 mins
-        },
-        "4": {
-            "start": "10:36AM",
-            "end": "11:14AM"
-        },
-        "5": {
-            "start": "11:18AM",
-            "end": "11:56AM"
-        },
-        "6": {
-            "start": "12:00AM",
-            "end": "12:38PM"
-        },
-        "7": {
-            "start": "12:42PM",
-            "end": "1:20PM"
-        },
-        "8": {
-            "start": "1:24PM",
-            "end": "2:02PM"
-        },
-        "9": {
-            "start": "2:06PM",
-            "end": "2:47PM"
-        }
-    }
-}
+VERSION = "0.1.0"
+
+schedules = {}
+json_path = Path(__file__).parent / "schedules"
+for schedule_json in json_path.glob("*.json"):
+    schedules[schedule_json.name.split(".")[0]] = json.loads((schedule_json.read_text()))
 
 class ScheduleApp(rumps.App):
     def __init__(self):
         super().__init__(
             name="Schedule App",
-            title="⏰",
+            title="",
             quit_button=None
         )
 
         self.menu = [
-            "Start Updating Schedule",
-            "Stop Updating Schedule",
             "Change Schedule",
             "About",
             rumps.separator,
             "Quit"
         ]
+
+        self.us_thread = threading.Thread(target=self.update_schedule_loop, daemon=True)
+        self.us_thread.start()
 
     def update_schedule_loop(self):
         while True:
@@ -122,14 +53,9 @@ class ScheduleApp(rumps.App):
                 self.title = "School's out!"
             time.sleep(0.5)
 
-    @rumps.clicked("Start Updating Schedule")
-    def start_updating(self, _):
-        self.us_thread = threading.Thread(target=self.update_schedule_loop, daemon=True)
-        self.us_thread.start()
-
     @rumps.clicked("About")
     def about(self, _):
-        rumps.alert("Schedule App v0.0.0\n\nAn easy way to see how much time is left until your next class!\n\nCreated by Aaron (GH: aaronwijes)")
+        rumps.alert(f"Schedule App v{VERSION}\n\nAn easy way to see how much time is left until your next class!\n\nCreated by Aaron (GH: aaronwijes)")
 
     @rumps.clicked("Quit")
     def quit_app(self, _):
