82d94791a774c6f3de0bd59d80bf08aedfbecf92
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-09-17 16:59:12 -0400

    feat: add the ability to refresh schedules from JSON files fix: remove threading in favor of rumps.Timer

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ import os
 import json
 import time
 import rumps
-import threading
 from datetime import datetime, timedelta
 from pathlib import Path
 
-VERSION = "0.3.0"
-
-schedules = {}
-json_path = Path(__file__).parent / "schedules"
-for schedule_json in json_path.glob("*.json"):
-    schedules[schedule_json.name.split(".")[0]] = json.loads((schedule_json.read_text()))
+VERSION = "0.4.0"
 
 def get_title(timeframe, period, seconds):
     title = f"{timeframe.upper()} ({period}): "
@@ -33,61 +27,81 @@ class ScheduleApp(rumps.App):
             quit_button=None
         )
 
-        self.options = sorted(list(schedules.keys()))
-        self.sub_items = {
-            opt: rumps.MenuItem(title=opt, callback=self.select_option)
-            for opt in self.options
-        }
+        self.sub_items = {}
+        self.schedules = {}
         self.selected_schedule = "default"
-        self.sub_items[self.selected_schedule].state = True
+
+        self.change_schedule = rumps.MenuItem(title="Change Schedule")
+        self.change_schedule.add(rumps.MenuItem("Loading..."))
 
         self.menu = [
-            ("Change Schedule", list(self.sub_items.values())),
+            self.change_schedule,
+            "Update Schedules",
             "About",
             rumps.separator,
             "Quit"
         ]
 
-        self.us_thread = threading.Thread(target=self.update_schedule_loop, daemon=True)
-        self.us_thread.start()
+        self.update_schedules(self.change_schedule)
+
+        self.timer = rumps.Timer(self.update_time_left, 0.5)
+        self.timer.start()
 
     def select_option(self, sender):
         self.sub_items[self.selected_schedule].state = False
         self.selected_schedule = sender.title
         sender.state = True
 
-    def update_schedule_loop(self):
-        while True:
-            now = datetime.now()
-            day = now.day
-            month = now.month
-            year = now.year
-            found_period = False
-            for period_id, period in schedules[self.selected_schedule].items():
-                if datetime.now().hour > datetime.strptime(period["start"], "%I:%M %p").hour:
-                    tomorrow = datetime.today() + timedelta(days=1)
-                    day = tomorrow.day
-                    month = tomorrow.month
-                    year = tomorrow.year
-
-                time_until_start = round((datetime.strptime(f"{month}/{day}/{year} {period["start"]}", "%m/%d/%Y %I:%M %p") - now).total_seconds())
-                time_until_end = round((datetime.strptime(f"{month}/{day}/{year} {period["end"]}", "%m/%d/%Y %I:%M %p") - now).total_seconds())
-                if time_until_start > 0:
-                    self.title = get_title("start", period_id, time_until_start)
-                    found_period = True
-                    break
-                elif time_until_end > 0:
-                    found_period = True
-                    self.title = get_title("end", period_id, time_until_end)
-                    break
-            if not found_period:
+    def update_time_left(self, _):
+        now = datetime.now()
+        day = now.day
+        month = now.month
+        year = now.year
+        found_period = False
+        for period_id, period in self.schedules[self.selected_schedule].items():
+            if datetime.now().hour > datetime.strptime(period["start"], "%I:%M %p").hour:
                 tomorrow = datetime.today() + timedelta(days=1)
                 day = tomorrow.day
                 month = tomorrow.month
                 year = tomorrow.year
-                time_until_start = round((datetime.strptime(f"{month}/{day}/{year} {schedules[self.selected_schedule][list(schedules[self.selected_schedule].keys())[0]]["start"]}", "%m/%d/%Y %I:%M %p") - now).total_seconds())
-                self.title = get_title("start", list(schedules[self.selected_schedule].keys())[0], time_until_start)
-            time.sleep(0.5)
+
+            time_until_start = round((datetime.strptime(f"{month}/{day}/{year} {period["start"]}", "%m/%d/%Y %I:%M %p") - now).total_seconds())
+            time_until_end = round((datetime.strptime(f"{month}/{day}/{year} {period["end"]}", "%m/%d/%Y %I:%M %p") - now).total_seconds())
+            if time_until_start > 0:
+                self.title = get_title("start", period_id, time_until_start)
+                found_period = True
+                break
+            elif time_until_end > 0:
+                found_period = True
+                self.title = get_title("end", period_id, time_until_end)
+                break
+        if not found_period:
+            tomorrow = datetime.today() + timedelta(days=1)
+            day = tomorrow.day
+            month = tomorrow.month
+            year = tomorrow.year
+            time_until_start = round((datetime.strptime(f"{month}/{day}/{year} {self.schedules[self.selected_schedule][list(self.schedules[self.selected_schedule].keys())[0]]["start"]}", "%m/%d/%Y %I:%M %p") - now).total_seconds())
+            self.title = get_title("start", list(self.schedules[self.selected_schedule].keys())[0], time_until_start)
+        time.sleep(0.5)
+
+    @rumps.clicked("Update Schedules")
+    def update_schedules(self, _):
+        self.schedules = {}
+        json_path = Path(__file__).parent / "schedules"
+        for schedule_json in json_path.glob("*.json"):
+            self.schedules[schedule_json.name.split(".")[0]] = json.loads((schedule_json.read_text()))
+
+        self.options = sorted(list(self.schedules.keys()))
+        self.sub_items = {
+            opt: rumps.MenuItem(title=opt, callback=self.select_option)
+            for opt in self.options
+        }
+        self.selected_schedule = "default"
+        self.sub_items[self.selected_schedule].state = True
+        
+        self.change_schedule.clear()
+        for item in self.sub_items.values():
+            self.change_schedule.add(item)
 
     @rumps.clicked("About")
     def about(self, _):
